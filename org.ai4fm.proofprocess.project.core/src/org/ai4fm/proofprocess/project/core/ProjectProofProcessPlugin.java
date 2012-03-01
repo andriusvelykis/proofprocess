cbf724435141b95f7a0e980e84fff3b4b3893593
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-01 12:32:22 +0000

    Added proofprocess.project.core plugin. It will contain generic functionality for project-based proof process models

---
+++ org.ai4fm.proofprocess.project.core/src/org/ai4fm/proofprocess/project/core/ProjectProofProcessPlugin.java
@@ -0,0 +1,74 @@
+package org.ai4fm.proofprocess.project.core;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+public class ProjectProofProcessPlugin extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.ai4fm.proofprocess.project.core"; //$NON-NLS-1$
+	
+	// The shared instance
+	private static ProjectProofProcessPlugin plugin;
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static ProjectProofProcessPlugin getDefault() {
+		return plugin;
+	}
+	
+	/**
+	 * Writes the message to the plug-in's log
+	 * 
+	 * @param message the text to write to the log
+	 */
+	public static void log(String message, Throwable exception) {
+		IStatus status = error(message, exception);
+		getDefault().getLog().log(status);
+	}
+	
+	public static void log(Throwable exception) {
+		log(exception.getMessage(), exception);
+	}
+	
+	/**
+	 * Returns a new <code>IStatus</code> for this plug-in
+	 */
+	public static IStatus error(Throwable exception) {
+		return error(exception.getMessage(), exception);
+	}
+	
+	/**
+	 * Returns a new <code>IStatus</code> for this plug-in
+	 */
+	public static IStatus error(String message, Throwable exception) {
+		if (message == null) {
+			message = ""; 
+		}		
+		return new Status(IStatus.ERROR, PLUGIN_ID, 0, message, exception);
+	}
+
+}
