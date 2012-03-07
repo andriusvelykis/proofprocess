c33af25e2688860f0356d5e8647d414567ab7965
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-07 11:46:24 +0000

    Logging and error status methods

--- org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/IsabelleProofPlugin.java
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/IsabelleProofPlugin.java
@@ -1,6 +1,8 @@
 package org.ai4fm.proofprocess.isabelle.core;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
 
 public class IsabelleProofPlugin extends Plugin {
@@ -36,6 +38,57 @@ public class IsabelleProofPlugin extends Plugin {
 	 */
 	public static IsabelleProofPlugin getDefault() {
 		return plugin;
+	}
+	
+	/**
+	 * Writes the message to the plug-in's log
+	 * 
+	 * @param message
+	 *            the text to write to the log
+	 * @param exception
+	 *            exception to capture in the log
+	 */
+	public static void log(String message, Throwable exception) {
+		IStatus status = error(message, exception);
+		getDefault().getLog().log(status);
+	}
+	
+	/**
+	 * Logs the exception to the plug-in's log. Exception's message is used as
+	 * the log message.
+	 * 
+	 * @param exception
+	 *            exception to capture in the log
+	 */
+	public static void log(Throwable exception) {
+		log(exception.getMessage(), exception);
+	}
+	
+	/**
+	 * Returns a new error {@code IStatus} for this plug-in.
+	 * 
+	 * @param exception
+	 *            exception to wrap in the error {@code IStatus}
+	 * @return the error {@code IStatus} wrapping the exception
+	 */
+	public static IStatus error(Throwable exception) {
+		return error(exception.getMessage(), exception);
+	}
+	
+	/**
+	 * Returns a new error {@code IStatus} for this plug-in.
+	 * 
+	 * @param message
+	 *            text to have as status message
+	 * @param exception
+	 *            exception to wrap in the error {@code IStatus}
+	 * @return the error {@code IStatus} wrapping the exception
+	 */
+	public static IStatus error(String message, Throwable exception) {
+		if (message == null) {
+			message = ""; 
+		}
+		return new Status(IStatus.ERROR, PLUGIN_ID, 0, message, exception);
 	}
 
 }
