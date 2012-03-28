aca53bf1488e95dafab32db0db7cb054218d2b77
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-03-28 10:25:56 +0100

    Test implementation of printing terms to ZML.
    The printing is actually quite slow (0.5-1 seconds per term), but I don't need to re-implement (re-model) the CZT term structure in ProofProcess. Will try adding this to EMF serialisation.

--- org.ai4fm.proofprocess.zeves.ui/src/org/ai4fm/proofprocess/zeves/ui/parse/TermParser.java
+++ org.ai4fm.proofprocess.zeves.ui/src/org/ai4fm/proofprocess/zeves/ui/parse/TermParser.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 import net.sourceforge.czt.eclipse.editors.zeditor.ZEditorUtil;
 import net.sourceforge.czt.eclipse.zeves.core.ZEvesResultConverter;
+import net.sourceforge.czt.parser.util.DeleteAnnVisitor;
+import net.sourceforge.czt.print.util.XmlString;
 import net.sourceforge.czt.session.CommandException;
+import net.sourceforge.czt.session.Key;
 import net.sourceforge.czt.session.SectionInfo;
+import net.sourceforge.czt.session.SectionInfoException;
 import net.sourceforge.czt.session.SectionManager;
 import net.sourceforge.czt.z.ast.Pred;
 import net.sourceforge.czt.zeves.response.ZEvesOutput;
@@ -23,6 +27,8 @@ import org.ai4fm.proofprocess.zeves.ui.ZEvesProofUIPlugin;
  */
 public class TermParser {
 
+	private static final String ZML_PRINT_KEY = ZEvesProofUIPlugin.PLUGIN_ID + ".term-zml";
+	
 	private static final ZEvesProofProcessFactory FACTORY = ZEvesProofProcessFactory.eINSTANCE;
 	
 	public static List<Term> parseGoals(SectionInfo sectInfo, String sectName, ZEvesOutput result) {
@@ -44,11 +50,17 @@ public class TermParser {
 				// a bit of a hack, will need to review whether a sectInfo could be enough eventually
 				
 				List<Term> goals = new ArrayList<Term>();
+				long start = System.currentTimeMillis();
 				Pred goalPred = ZEvesResultConverter.parseZEvesPred((SectionManager) sectInfo, sectName, goalStr);
+				System.out.println("Parsing term: " + (System.currentTimeMillis() - start));
+				
+				start = System.currentTimeMillis();
+				String zmlPred = printZml((SectionManager) sectInfo, goalPred);
+				System.out.println("Printing term to ZML: " + (System.currentTimeMillis() - start));
 				
 				// TODO convert the parsed predicate
 				UnparsedTerm term = FACTORY.createUnparsedTerm();
-				term.setDisplay(goalStr);
+				term.setDisplay(zmlPred);
 				goals.add(term);
 				
 				return goals;
@@ -70,6 +82,23 @@ public class TermParser {
 		UnparsedTerm term = FACTORY.createUnparsedTerm();
 		term.setDisplay(goalStr);
 		return Collections.<Term>singletonList(term);
+	}
+	
+	private static String printZml(SectionManager sectInfo, net.sourceforge.czt.base.ast.Term cztTerm)
+			throws CommandException {
+		
+		// strip all annotations (e.g. LocAnn, etc) during serialization
+		cztTerm.accept(new DeleteAnnVisitor());
+		
+		SectionManager cloned = sectInfo.clone();
+		
+		Key<net.sourceforge.czt.base.ast.Term> termKey = new Key<net.sourceforge.czt.base.ast.Term>(
+				ZML_PRINT_KEY, net.sourceforge.czt.base.ast.Term.class);
+		
+		cloned.put(termKey, cztTerm);
+		XmlString printed = cloned.get(new Key<XmlString>(ZML_PRINT_KEY, XmlString.class));
+		
+		return printed.toString();
 	}
 	
 }
