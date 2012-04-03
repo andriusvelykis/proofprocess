62aa966485c50b944525b5885eef1cd1c662f35a
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-04-03 08:35:42 +0100

    Project for generic ProofProcess code

---
+++ org.ai4fm.proofprocess.core/src/org/ai4fm/proofprocess/core/ProofProcessPlugin.java
@@ -0,0 +1,30 @@
+package org.ai4fm.proofprocess.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ProofProcessPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		ProofProcessPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		ProofProcessPlugin.context = null;
+	}
+
+}
