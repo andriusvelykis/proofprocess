1cc3f93a7151443214be8e16cd44d2e7e57eab48
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-05-23 15:44:50 +0100

    proof store provider to allow lazy resolution of proof store (it can take a while to load)

---
+++ org.ai4fm.proofprocess.core/src/org/ai4fm/proofprocess/core/store/IProofStoreProvider.scala
@@ -0,0 +1,7 @@
+package org.ai4fm.proofprocess.core.store
+
+trait IProofStoreProvider {
+
+  def store: IProofStore
+  
+}
---
+++ org.ai4fm.proofprocess.project.core/src/org/ai4fm/proofprocess/project/core/store/ProjectProofStoreProvider.scala
@@ -0,0 +1,17 @@
+package org.ai4fm.proofprocess.project.core.store
+
+import org.ai4fm.proofprocess.core.store.IProofStore
+import org.ai4fm.proofprocess.core.store.IProofStoreProvider
+import org.ai4fm.proofprocess.project.core.ProofManager
+
+import org.eclipse.core.resources.IProject
+
+class ProjectProofStoreProvider(private val project: IProject) extends IProofStoreProvider {
+
+  lazy val store: IProofStore = {
+    val proofProject = ProofManager.getProofProject(project, null);
+    
+    new ProjectProofStore(proofProject)
+  }
+  
+}
