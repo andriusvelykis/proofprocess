af179bfad5aac34252bb9535ab69cae24097e92a
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-05-24 10:33:57 +0100

    basic tree viewer for ProofProcess data loaded via IProofStoreProvider. It is a port of a ProofProcess Z/EVES view with less functionality at the moment. Allows displaying the tree without having to run Z/EVES plugins

---
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/views/PProcessPage.scala
@@ -0,0 +1,85 @@
+package org.ai4fm.proofprocess.ui.views
+
+import org.ai4fm.proofprocess.core.store.IProofStoreProvider
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.ui.part.Page
+import org.eclipse.swt.widgets.Control
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory
+import org.eclipse.swt.SWT
+import org.eclipse.jface.layout.GridLayoutFactory
+import org.eclipse.swt.widgets.Label
+import org.eclipse.jface.viewers.TreeViewer
+import org.eclipse.jface.layout.GridDataFactory
+import org.eclipse.jface.action.MenuManager
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider
+import org.eclipse.core.runtime.jobs.Job
+import org.eclipse.core.runtime.Status
+import org.eclipse.core.runtime.IProgressMonitor
+import org.eclipse.core.runtime.IStatus
+import org.ai4fm.proofprocess.core.store.IProofStore
+
+class PProcessPage(private val proofStoreProvider: IProofStoreProvider) extends Page {
+
+  private val adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+  
+  private var main: Composite = _
+  private var treeViewer: TreeViewer = _
+
+  override def createControl(parent: Composite) {
+
+    main = new Composite(parent, SWT.NONE);
+    main.setLayout(GridLayoutFactory.fillDefaults.extendedMargins(0, 0, 2, 0).create);
+
+    treeViewer = new TreeViewer(main, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+    treeViewer.getControl().setLayoutData(GridDataFactory.fillDefaults.grab(true, true).create);
+
+//    val groupAttemptsAction = new GroupAttemptsAction();
+//    // add context menu
+//    val mgr = new MenuManager();
+//    mgr.add(groupAttemptsAction);
+//    val tree = treeViewer.getTree();
+//    tree.setMenu(mgr.createContextMenu(tree));
+    
+    val contentProvider = new AdapterFactoryContentProvider(adapterFactory) {
+     override def getElements(root: Object): Array[Object] = root match {
+       // get proofs and intents from the store to display as root elements
+       case store: IProofStore => store.getProofs.toArray ++ store.getIntents.toArray
+       case _ => super.getElements(root)
+     } 
+    }
+
+    treeViewer.setContentProvider(contentProvider);
+    treeViewer.setLabelProvider(new AdapterFactoryLabelProvider(adapterFactory));
+
+    // load the proof process in a separate job, otherwise it delays the startup
+    val loadJob = new Job("Loading proof process") {
+
+      override protected def run(monitor: IProgressMonitor): IStatus = {
+
+        // load the proof store if needed
+        val proofStore = proofStoreProvider.store
+
+        // set tree input back in the UI thread
+        main.getDisplay asyncExec {() => treeViewer.setInput(proofStore)}
+
+        Status.OK_STATUS
+      }
+    }
+    loadJob.setPriority(Job.DECORATE);
+    loadJob.schedule();
+  }
+  
+  implicit private def runnable(f: () => Unit): Runnable =
+    new Runnable() { def run() = f() }
+
+  override def getControl = main
+
+  override def setFocus = treeViewer.getControl.setFocus
+  
+  override def dispose {
+    adapterFactory.dispose
+	super.dispose
+  }
+
+}
---
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/views/PProcessView.scala
@@ -0,0 +1,67 @@
+package org.ai4fm.proofprocess.ui.views
+
+import org.ai4fm.proofprocess.core.store.IProofStoreProvider
+
+import org.eclipse.core.resources.IResource
+import org.eclipse.ui.IEditorPart
+import org.eclipse.ui.IWorkbenchPart
+import org.eclipse.ui.ide.ResourceUtil
+import org.eclipse.ui.part.IPage
+import org.eclipse.ui.part.MessagePage
+import org.eclipse.ui.part.PageBook
+
+
+class PProcessView extends ScalaPageBookView {
+
+  override protected def createDefaultPage(book: PageBook): IPage = {
+    val messagePage = new MessagePage
+    initPage(messagePage)
+
+    messagePage.setMessage("Proof process is not available.");
+    messagePage.createControl(book);
+    messagePage;
+  }
+
+  override protected def doCreatePage0(part: IWorkbenchPart): IPage = {
+    // must resolve without None
+    val proofStoreProvider = getProofStore(part).get
+
+    // create a page with the proof store provider
+    val proofProcessPage = new PProcessPage(proofStoreProvider)
+    initPage(proofProcessPage)
+    proofProcessPage.createControl(getPageBook())
+
+    proofProcessPage
+  }
+
+  protected def doDestroyPage(part: IWorkbenchPart, page: IPage) =
+    page.dispose()
+
+  override protected def getBootstrapPart(): IWorkbenchPart = {
+    val part = Option(getSite.getPage) flatMap { p => Option(p.getActivePart) } filter { isImportant }
+    part.orNull
+  }
+
+  override protected def isImportant(part: IWorkbenchPart) = getProofStore(part).isDefined
+
+  private def getProofStore(part: IWorkbenchPart): Option[IProofStoreProvider] =
+    // get the resource and try to resolve the proof store via adapter
+    getResource(part) flatMap { resource =>
+      // force load adapters (likely in different plugins, which may be not loaded yet)
+      Option(ResourceUtil.getAdapter(resource, classOf[IProofStoreProvider], true).asInstanceOf[IProofStoreProvider])
+    }
+  
+  private def getResource(part: IWorkbenchPart): Option[IResource] =
+    part match {
+      case editor: IEditorPart => {
+        // cast to Option otherwise wrong ResourceUtil.getFile() is chosen
+        val input = editor.getEditorInput().asInstanceOf[Object]
+
+        // try resolving as file (a number of options there)
+        // otherwise, try at least resource
+        Option(ResourceUtil.getFile(input)) orElse Option(ResourceUtil.getResource(input))
+      }
+      case _ => None
+    }
+
+}
