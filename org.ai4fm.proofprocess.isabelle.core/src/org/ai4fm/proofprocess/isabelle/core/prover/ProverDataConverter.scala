551510ad8c99219fda47f6720ca31e0ee578fdde
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-06-11 22:23:19 +0100

    moved XML encoding of ProverData to ProverData object

--- org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/prover/ProverDataConverter.scala
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/prover/ProverDataConverter.scala
@@ -1,8 +1,5 @@
 package org.ai4fm.proofprocess.isabelle.core.prover
 
-import isabelle.Markup
-import isabelle.Term_XML
-import isabelle.XML
 import org.ai4fm.proofprocess.Attempt
 import org.ai4fm.proofprocess.ProofDecor
 import org.ai4fm.proofprocess.ProofElem
@@ -104,101 +101,5 @@ object ProverDataConverter {
       case t: PPIsaTerm => IsaTerm(t.getTerm)
       case s: DisplayTerm => StrTerm(s.getDisplay)
     })
-
-    
-  object Encode {
-
-    import XML._
-    
-    def encodeTerm(term: TermRef) = term match {
-      case IsaTerm(t) => elem ("Term", Term_XML.Encode.term(t))
-      case StrTerm(s) => Elem(Markup("TermStr", List(("val", s))), Nil)
-    }
-
-    def encodeStringPair(s: String, t: TermRef) = Elem(Markup("Pair", List(("name", s))), List(encodeTerm(t)))
-
-    def encodeAssocL(als: List[(String, TermRef)]) = elem("AssocList", als.map(Function.tupled(encodeStringPair)))
-
-    def encodePS(state: ProofState) = elem("PS", List(encodeAssocL(state.assumptions), encodeTerm(state.goal)))
-
-    def encodeThmName(thmName: ThmName) = thmName match {
-      case Thm(s) => Elem(Markup("Thm", List(("name", s))), Nil)
-      case Hyp(s) => Elem(Markup("Hyp", List(("name", s))), Nil)
-    }
-    
-    def encodeOpts(opt: Option[Body]) = opt match {
-      case None => elem("NONE")
-      case Some(body) => elem("SOME", body)
-    }
-    
-    def encodeOpt(opt: Option[Tree]) = encodeOpts(opt.map(List(_)))
-    
-    def thmElem(name: String, thm: ThmName) = elem(name, List(encodeThmName(thm)))
-    def thmsElem(name: String, thms: List[ThmName]) = elem(name, thms map encodeThmName)
-    
-    def optThmElem(name: String, thm: Option[ThmName]) = 
-      elem(name, List(encodeOpt(thm map encodeThmName)))
-    def optThmsElem(name: String, thms: Option[List[ThmName]]) =
-      elem(name, List(encodeOpts(thms map { _ map encodeThmName })))
-    
-    def encodeTac(tac: ATac) = tac match {
-      case Auto(simp, intro, dest) => elem("Auto", List(
-          thmsElem("Simp", simp),
-          thmsElem("Intro", intro),
-          thmsElem("Dest", dest)))
-      case Simp(add, del, only) => elem("Simp", List(
-          thmsElem("Add", add),
-          thmsElem("Del", del),
-          optThmsElem("Only", only)))
-      case Conj(term) => elem("Conj", List(encodeTerm(term)))
-      case Blast(dest, intro) => elem("Blast", List(
-          thmsElem("Intro", intro),
-          thmsElem("Dest", dest)))
-      case Force(simp, intro, dest) => elem("Force", List(
-          thmsElem("Simp", simp),
-          thmsElem("Intro", intro),
-          thmsElem("Dest", dest)))
-      case Metis(thms) => thmsElem("Metis", thms)
-      case Induction(rule, arg) => elem("Induction", List(
-          optThmElem("Rule", rule),
-          elem("Arg", List(encodeOpt(arg map encodeTerm)))))
-      case UnknownTac(s) => Elem(Markup("UnknownTac", List(("val", s))), Nil)
-    }
-    
-    def encodeMeth(meth: Meth): Tree = meth match {
-      case Rule(thm) => thmElem("Rule", thm)
-      case Erule(asm, thm) => elem("Erule", List(
-          optThmElem("Assumption", asm),
-          thmElem("Theorem", thm)))
-      case Frule(asm, thm) => elem("Frule", List(
-          optThmElem("Assumption", asm),
-          thmElem("Theorem", thm)))
-      case SubstThm(thm) => thmElem("Subst_thm", thm)
-      case SubstAsmThm(asm, thm) => elem("Subst_asm_thm", List(
-          thmElem("Assumption", asm),
-          thmElem("Theorem", thm)))
-      case SubstUsingAsm(thm) => thmElem("Subst_using_asm", thm)
-      case Case(term) => elem("Subst_using_asm", List(encodeTerm(term)))
-      case Tactic(tac) => elem("Tactic", List(encodeTac(tac)))
-      case Using(thms, meth) => elem("Using", List(
-          thmsElem("Thms", thms),
-          elem("Method", List(encodeMeth(meth)))))
-      case Unknown(str) => Elem(Markup("Unknown", List(("name", str))), Nil)
-    }
-    
-    def encodeWhy(w: Why) = Elem(Markup("Why", List(("why_info", w.why))), List(encodeMeth(w.meth)))
-    
-    def encodePT(pt: ProofTree): Tree = pt match {
-      case Gap() => elem("Gap")
-      case Proof(why, goals, cont) => elem("Proof", List(
-          encodeWhy(why),
-          elem("Goals", goals map encodePS),
-          encodePT(cont)))
-      case Failure(failures, valid) => elem("Failure", List(
-          elem("Failures", failures map encodePT),
-          elem("Valid", List(encodeOpt(valid map encodePT)))))
-    }
-    
-  }
     
 }
