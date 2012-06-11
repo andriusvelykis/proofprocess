9af3563c09e6d499ca3bebebb66584f7ad074df4
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-06-11 11:48:45 +0100

    basic support for Attempt conversion

--- org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/prover/ProverDataConverter.scala
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/prover/ProverDataConverter.scala
@@ -1,12 +1,20 @@
 package org.ai4fm.proofprocess.isabelle.core.prover
 
+import org.ai4fm.proofprocess.Attempt
+import org.ai4fm.proofprocess.ProofDecor
+import org.ai4fm.proofprocess.ProofElem
+import org.ai4fm.proofprocess.ProofEntry
+import org.ai4fm.proofprocess.ProofParallel
+import org.ai4fm.proofprocess.ProofSeq
 import org.ai4fm.proofprocess.Term
 import org.ai4fm.proofprocess.isabelle.IsabelleCommand
+import org.ai4fm.proofprocess.isabelle.IsabelleTrace
 import org.ai4fm.proofprocess.isabelle.IsaTerm
 import org.ai4fm.proofprocess.isabelle.NameTerm
 import org.ai4fm.proofprocess.isabelle.core.parse.{IsaCommands => isa}
 import org.ai4fm.proofprocess.isabelle.core.prover.ProverData._
 import scala.collection.JavaConversions._
+
 
 /** Converters from ProofProcess data to ProverData objects to send to Isabelle.
   *  
@@ -15,6 +23,47 @@ import scala.collection.JavaConversions._
 object ProverDataConverter {
   
   private type ITerm = isabelle.Term.Term
+  
+  def attempt(attempt: Attempt): ProofTree = {
+    val rootElem = attempt.getProof
+    proofTree(rootElem, Gap())
+  }
+  
+  private def proofTree(elem: ProofElem, cont: ProofTree) : ProofTree = elem match {
+    
+    case e: ProofEntry => {
+      val (why, goals) = entry(e)
+      val goalStates = goals.map(nameGoalState)
+      Proof(why, goalStates, cont)
+    }
+    case d: ProofDecor => proofTree(d.getEntry, cont)
+    case s: ProofSeq => s.getEntries.foldRight(cont)(proofTree)
+    // TODO special handling for parallel?
+    case p: ProofParallel => p.getEntries.foldRight(cont)(proofTree)
+  }
+  
+  def entry(entry: ProofEntry): (Why, List[ITerm]) = {
+    val intentName = entry.getInfo.getIntent.getName
+    
+    val proofStep = entry.getProofStep
+    // how about out goals?
+    val inGoals = terms(proofStep.getInGoals.toList)
+    
+    val commandOpt = proofStep.getTrace match {
+      case isaTrace: IsabelleTrace => Some(command(isaTrace.getCommand))
+      case _ => None
+    }
+    
+    val cmd = commandOpt getOrElse {Unknown(String.valueOf(proofStep.getTrace))}
+    
+    (Why(intentName, cmd), inGoals)
+  }
+  
+  private def nameGoalState(term: ITerm): ProofState = {
+    // for now just pack the term into the proof state without naming assumptions/fixes
+    // TODO name assumptions
+    ProofState(Nil, Nil, term)
+  }
   
   def command(cmd: IsabelleCommand): Meth = {
     val res = cmd match {
