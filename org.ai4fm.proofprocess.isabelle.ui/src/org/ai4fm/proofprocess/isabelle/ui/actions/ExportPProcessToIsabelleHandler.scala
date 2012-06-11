b4f6b17f3c06fc9b17826804ea668d17f62d27f2
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-06-11 11:49:24 +0100

    placeholder for exporting ProofProcess data - currently just converting and printing Attempts

--- org.ai4fm.proofprocess.isabelle.ui/src/org/ai4fm/proofprocess/isabelle/ui/actions/ExportPProcessToIsabelleHandler.scala
+++ org.ai4fm.proofprocess.isabelle.ui/src/org/ai4fm/proofprocess/isabelle/ui/actions/ExportPProcessToIsabelleHandler.scala
@@ -1,11 +1,14 @@
 package org.ai4fm.proofprocess.isabelle.ui.actions
 
+import org.ai4fm.proofprocess.Attempt
 import org.eclipse.core.commands.AbstractHandler
 import org.eclipse.core.commands.ExecutionEvent
 import org.eclipse.core.commands.ExecutionException
 import org.eclipse.jface.viewers.IStructuredSelection
 import org.eclipse.ui.handlers.HandlerUtil
 import scala.collection.JavaConversions._
+import org.ai4fm.proofprocess.isabelle.core.prover.ProverDataConverter
+
 
 class ExportPProcessToIsabelleHandler extends AbstractHandler {
 
@@ -17,8 +20,12 @@ class ExportPProcessToIsabelleHandler extends AbstractHandler {
     
     val selected = selection map (_.iterator.toList )
     
-    selected foreach { elems =>
-      println(elems)
+    val attempts = selected map { _.collect{ case a: Attempt => a } }
+
+    attempts foreach {
+      _.foreach { attempt =>
+        println("Export attempt: " + ProverDataConverter.attempt(attempt))
+      }
     }
 
     // return value is reserved for future APIs
