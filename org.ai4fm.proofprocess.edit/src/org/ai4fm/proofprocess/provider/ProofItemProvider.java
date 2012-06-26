57f94f528ee47a72687f6eb774715333b3a26bc2
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-06-26 10:23:20 +0100

    turnstile icon for proof objects

--- org.ai4fm.proofprocess.edit/src/org/ai4fm/proofprocess/provider/ProofItemProvider.java
+++ org.ai4fm.proofprocess.edit/src/org/ai4fm/proofprocess/provider/ProofItemProvider.java
@@ -126,14 +126,14 @@ public class ProofItemProvider
 	}
 
 	/**
-	 * This returns Proof.gif.
+	 * This returns Proof.png.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/Proof"));
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/Proof.png"));
 	}
 
 	/**
