21093b3527b58a593c3704f5aaafc73ef5259f1c
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-06-28 13:51:45 +0100

    initial image definitions

---
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/PProcessImages.scala
@@ -0,0 +1,33 @@
+package org.ai4fm.proofprocess.ui
+
+import java.net.MalformedURLException
+import java.net.URL
+
+import org.eclipse.jface.resource.ImageDescriptor
+
+/** ProofProcess image definitions.
+  * 
+  * When images are used in label providers (e.g. where Image) is required, they must be disposed manually.
+  * For convenience, [[org.eclipse.jface.resource.ResourceManager]] could be used. 
+  * 
+  * @author Andrius Velykis
+  */
+object PProcessImages {
+
+  private lazy val ICON_BASE_URL = PProcessUIPlugin.plugin.getBundle.getEntry("icons/")
+
+  val MISSING_ICON = ImageDescriptor.getMissingImageDescriptor
+
+  lazy val GOAL_IN = create("goal_in.gif")
+  lazy val GOAL_OUT = create("goal_out.gif")
+
+  private def create(iconPath: String) = {
+    try {
+      val url = new URL(ICON_BASE_URL, iconPath)
+      ImageDescriptor.createFromURL(url)
+    } catch {
+      case _: MalformedURLException => MISSING_ICON
+    }
+  }
+
+}
