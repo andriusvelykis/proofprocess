255314f239f1901b895712748c8e13eb16f7916f
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-07-24 08:20:28 +0100

    new plugin for Machine Learning data provision out of ProofProcess data;
    implementation placeholders only at the moment

---
+++ org.ai4fm.proofprocess.mlearning.data/src/org/ai4fm/proofprocess/mlearning/data/MLearningDataProvider.scala
@@ -0,0 +1,11 @@
+package org.ai4fm.proofprocess.mlearning.data
+
+import org.ai4fm.proofprocess.ProofStore
+
+object MLearningDataProvider {
+
+  def extractData(store: ProofStore): String = {
+    "TODO"
+  }
+  
+}
