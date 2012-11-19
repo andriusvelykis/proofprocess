9d8e802b880b36cbcfd97cd6f470b78249576b41
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-11-19 21:47:51 +0000

    support empty steps and "new goal" steps; clearer code structure

--- org.ai4fm.proofprocess.core/src/org/ai4fm/proofprocess/core/analysis/GoalGraphMatcher.scala
+++ org.ai4fm.proofprocess.core/src/org/ai4fm/proofprocess/core/analysis/GoalGraphMatcher.scala
@@ -67,24 +67,86 @@ object GoalGraphMatcher {
     val nodeEntry = node(nodeInfo, changedIn, changedOut)
     
     // add the entry itself to the graph
-    val entryGraph = context.graph + nodeEntry
+    val entryContext = context.copy(graph = context.graph + nodeEntry)
+
+    (changedIn, changedOut) match {
+
+      // a step that did not affect proof state (e.g. rearranging of goals?)
+      case (Nil, Nil) => linkStepEmpty(entryContext, nodeEntry)
+
+      // no incoming goals affected, but new goals introduced
+      case (Nil, out) => linkStepNewGoal(entryContext, nodeEntry, out)
+
+      // normal step changing some goals to other ones (or finishing them)
+      case (in, out) => linkStepChange(entryContext, nodeEntry, in, out)
+    }
+  }
+
+  /**
+   * Handles the step that did not affect proof state (e.g. rearranging of goals?).
+   * Links it to the last available branch and adds to the branch list. This will result
+   * in a direct merge further, with this step being a parallel, e.g.
+   *    A
+   *   / \
+   *   B  |
+   *   \ /
+   *    C
+   * 
+   * Here the empty step is marked as B, while A and C are normal steps.
+   */
+  private def linkStepEmpty[N, T](context: LinkContext[N, T], nodeEntry: N): LinkContext[N, T] = {
     
+    val newBranches = Branch(nodeEntry, List[T]()) :: context.branches
+    
+    context.branches match {
+      // there are no outstanding branches, this is a first step, so mark it as a root
+      case Nil => LinkContext(context.graph, nodeEntry :: context.roots, newBranches)
+      
+      // if nesting under a branch, it is not a root
+      case b :: bs => LinkContext(
+          context.graph + (b.end ~> nodeEntry),
+          context.roots,
+          newBranches)
+    }
+  }
+
+  /**
+   * Handles the step that only introduced new goals but did not affect any existing goals.
+   * Adds it as a root step (will be parallel to other top branches).
+   */
+  private def linkStepNewGoal[N, T](context: LinkContext[N, T],
+                                    nodeEntry: N,
+                                    out: List[T]): LinkContext[N, T] =
+    LinkContext(
+      context.graph,
+      nodeEntry :: context.roots,
+      Branch(nodeEntry, out) :: context.branches)
+
+  /**
+   * Handles the step that only introduced new goals but did not affect any existing goals.
+   * Adds it as a root step (will be parallel to other top branches).
+   */
+  private def linkStepChange[N, T](context: LinkContext[N, T],
+                                   nodeEntry: N,
+                                   in: List[T],
+                                   out: List[T]): LinkContext[N, T] = {
+
     // diff the goals with the branches: find which branches satisfy which goals
-    // note: only `changedIn` goals are checked, since only they were actually affected by the step
-    val (matchedNodes, remainingGoals, remainingBranches) = 
-      branchesWithGoals(context.branches, changedIn)
+    // note: only "in" goals are checked, since only they were actually affected by the step
+    val (matchedNodes, remainingGoals, remainingBranches) = branchesWithGoals(context.branches, in)
 
     // if there are unmatched "in" goals, it means they came from the root goal
     // so mark the node as one of the roots
     val newRoots = prependIfNotEmpty(remainingGoals, context.roots)(_ => nodeEntry)
 
     // create a new branch for the node with its "out" goals (if there are outstanding goals)
-    val newBranches = prependIfNotEmpty(changedOut, remainingBranches)(Branch(nodeEntry, _))
+    val newBranches = prependIfNotEmpty(out, remainingBranches)(Branch(nodeEntry, _))
     
     // link matched nodes to the current node in the graph
-    val newGraph = (matchedNodes foldLeft entryGraph)( (g, m) => g + (m ~> nodeEntry) )
+    val newGraph = (matchedNodes foldLeft context.graph)((g, m) => g + (m ~> nodeEntry))
     
     LinkContext(newGraph, newRoots, newBranches)
+
   }
 
   /**
@@ -99,7 +161,7 @@ object GoalGraphMatcher {
                                       goals: List[T]): (List[N], List[T], List[Branch[N, T]]) =
     (branches, goals) match {
 
-      // if there are no more ounstanding goals, return the remaining branches
+      // if there are no more outstanding goals, return the remaining branches
       case (_, Nil) => (Nil, Nil, branches)
 
       // if there are no more branches, return the outstanding goals
@@ -113,7 +175,15 @@ object GoalGraphMatcher {
         val (finalNodes, finalGoals, finalBranches) = branchesWithGoals(bs, remainingGoals)
         
         // if there are matching goals, mark the branch node as matching
-        val newNodes = prependIfNotEmpty(matching, finalNodes)(_ => b.end)
+//        val newNodes = prependIfNotEmpty(matching, finalNodes)(_ => b.end)
+        // to support no-change steps as parallels, instead mark as matching when no goals are
+        // remaining in the branch as well
+        // (no-change steps would be indicated as empty-goal branches)
+        val newNodes = if (!matching.isEmpty || remainingBranch.isEmpty) {
+          b.end :: finalNodes
+        } else {
+          finalNodes
+        }
         
         // if there are remaining branch goals, keep the branch with them
         val newBranches = prependIfNotEmpty(remainingBranch, finalBranches)(Branch(b.end, _))
