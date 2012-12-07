cc8376d53f20c73bb544028e92dc9f6a026641c9
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2012-12-07 09:31:14 +0000

    basic tests for VF2 implementation

---
+++ lib/graph-isomorphism/src/test/scala/org/ai4fm/graph/isomorphism/VF2IsomorphismTest.scala
@@ -0,0 +1,77 @@
+package org.ai4fm.graph.isomorphism
+
+import org.junit.Assert._
+import org.junit.Test
+import scalax.collection.GraphEdge.DiEdge
+import scalax.collection.GraphPredef._
+import scalax.collection.immutable.Graph
+import scala.util.Random
+
+
+/**
+  * @author Andrius Velykis 
+  */
+class VF2IsomorphismTest {
+  
+  @Test
+  def graphItself1() {
+    
+    val graph1 = Graph(1 ~> 2, 2 ~> 3, 2 ~> 4)
+    val isom = VF2Isomorphism(graph1, graph1)
+    assertTrue(isom.isIsomorphism)
+    assertEquals(2, isom.isomorphisms.size)
+  }
+  
+  @Test
+  def graphItselfUndirected1() {
+    
+    val graph1 = Graph(1 ~ 2, 2 ~ 3, 2 ~ 4)
+    val isom = VF2Isomorphism(graph1, graph1)
+    assertTrue(isom.isIsomorphism)
+    // 6 isomorphisms for undirected, since we get a star-like graph
+    assertEquals(6, isom.isomorphisms.size)
+  }
+  
+  @Test
+  def disconnectedItself1() {
+    
+    val graph1 = Graph(1 ~> 2, 2 ~> 3, 2 ~> 4, 5 ~> 6)
+    val isom = VF2Isomorphism(graph1, graph1)
+    assertTrue(isom.isIsomorphism)
+    assertEquals(2, isom.isomorphisms.size)
+  }
+  
+  @Test
+  def cycleItself1() {
+    
+    val graph1 = Graph(1 ~> 2, 2 ~> 3, 3 ~> 1)
+    val isom = VF2Isomorphism(graph1, graph1)
+    assertTrue(isom.isIsomorphism)
+    assertEquals(3, isom.isomorphisms.size)
+  }
+  
+  @Test
+  def subgraph1() {
+    
+    val graph1 = Graph(1 ~> 2, 2 ~> 3, 2 ~> 4, 4 ~> 5, 3 ~> 5)
+    val subgraph1 = Graph("A" ~> "B", "B" ~> "D", "A" ~> "C", "C" ~> "D")
+    val isom = VF2Isomorphism(graph1, subgraph1)
+    assertTrue(isom.isIsomorphism)
+    assertEquals(2, isom.isomorphisms.size)
+  }
+  
+  @Test
+  def randomGraphItself() {
+    
+    val graph1 = ((1 to 100) foldLeft Graph(0 ~> 1)) {
+      (g, _) => {
+        g + (Random.nextInt(100) ~> Random.nextInt(100)) 
+      }
+    }
+    
+    val isom = VF2Isomorphism(graph1, graph1)
+    assertTrue(isom.isIsomorphism)
+  }
+  
+  
+}
