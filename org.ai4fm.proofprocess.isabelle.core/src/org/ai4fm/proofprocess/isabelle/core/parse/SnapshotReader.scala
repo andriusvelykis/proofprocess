aaa4904a7aa119489f21452019d9827cac0b6877
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-03-04 20:51:42 +0000

    updated to match Isabelle/Scala 2013 API

--- org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/parse/SnapshotReader.scala
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/parse/SnapshotReader.scala
@@ -125,7 +125,7 @@ object SnapshotReader {
     // first of all go backwards and collect everything before the target command
     // stop when the proof start command is reached
     var lastProofStart = false
-    val beforeCmdsRev = commands.reverse_iterator(targetCommand).map(commandState).takeWhile(state => {
+    val beforeCmdsRev = commands.reverse.iterator(targetCommand).map(commandState).takeWhile(state => {
       val prevStart = lastProofStart
       lastProofStart = isProofStart(state)
       // take while previous is not proof start
