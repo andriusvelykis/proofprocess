99db1d5ab0f7342d73f81c697f68c2b32d804f42
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-04-17 23:43:31 +0100

    adapting some Isabelle term manipulation

---
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/logic/Logic.scala
@@ -0,0 +1,33 @@
+package org.ai4fm.proofprocess.isabelle.core.logic
+
+import isabelle.Term._
+
+
+/**
+ * Various methods to manipulate Isabelle terms.
+ * 
+ * (adapted from Pure/logic.ML)
+ * 
+ * @author Andrius Velykis
+ */
+object Logic {
+
+  private val IMP = "HOL.Trueprop"
+
+  /**
+   * A1==>...An==>B  goes to  [A1,...,An], where B is not an implication
+   */
+  def strip_imp_prems(term: Term): List[Term] = term match {
+    case (App(Const(IMP, _), App(a, b))) => a :: strip_imp_prems(b)
+    case _ => Nil
+  }
+
+  /**
+   * A1==>...An==>B  goes to B, where B is not an implication
+   */
+  def strip_imp_concl(term: Term): Term = term match {
+    case (App(Const(IMP, _), App(a, b))) => strip_imp_concl(b)
+    case a => a
+  }
+
+}
