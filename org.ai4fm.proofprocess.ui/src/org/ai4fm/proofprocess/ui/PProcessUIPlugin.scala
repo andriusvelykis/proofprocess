717030c32cec177d308a8f30ae8640ff5450b756
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-05-13 13:16:33 +0100

    log/error methods

--- org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/PProcessUIPlugin.scala
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/PProcessUIPlugin.scala
@@ -1,5 +1,6 @@
 package org.ai4fm.proofprocess.ui
 
+import org.eclipse.core.runtime.{IStatus, Status}
 import org.eclipse.ui.plugin.AbstractUIPlugin
 
 
@@ -10,7 +11,22 @@ object PProcessUIPlugin {
   def plugin = instance
   
   def prefStore = plugin.getPreferenceStore
+
+  def log(status: IStatus) = plugin.getLog.log(status)
+
+  /** Returns a new error `IStatus` for this plug-in.
+    *
+    * @param message    text to have as status message
+    * @param exception  exception to wrap in the error `IStatus`
+    * @return  the error `IStatus` wrapping the exception
+    */
+  def error(ex: Option[Throwable] = None, msg: Option[String] = None): IStatus = {
     
+    // if message is not given, try to use exception's
+    val message = msg orElse { ex.flatMap(ex => Option(ex.getMessage)) }
+    
+    new Status(IStatus.ERROR, plugin.pluginId, 0, message.orNull, ex.orNull);
+  }
 }
 
 class PProcessUIPlugin extends AbstractUIPlugin {
