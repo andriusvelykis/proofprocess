36a42167ad58b962df0b281393fabfdbc3681faf
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-05-13 11:57:46 +0100

    placeholder for a dialog to mark PP features

---
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/actions/MarkFeaturesHandler.scala
@@ -0,0 +1,47 @@
+package org.ai4fm.proofprocess.ui.actions
+
+import org.ai4fm.proofprocess.ProofEntry
+import org.ai4fm.proofprocess.ui.features.MarkFeaturesDialog
+import org.eclipse.core.commands.{AbstractHandler, ExecutionEvent, ExecutionException}
+import org.eclipse.jface.viewers.IStructuredSelection
+import org.eclipse.ui.handlers.HandlerUtil
+
+
+/**
+ * Opens a dialog to mark features for the given selection.
+ *
+ * @author Andrius Velykis
+ */
+class MarkFeaturesHandler extends AbstractHandler {
+
+  @throws(classOf[ExecutionException])
+  override def execute(event: ExecutionEvent): AnyRef = {
+
+    // get the selected element if available
+    val selection = selectedElem(event)
+
+    selection match {
+      case Some(e: ProofEntry) => handleProofEntrySelected(event, e)
+      case _ => // ignore
+    }
+
+    // return value is reserved for future APIs
+    null
+  }
+
+  private def selectedElem(event: ExecutionEvent): Option[Any] =
+    Option(HandlerUtil.getCurrentSelection(event)) match {
+      case Some(ss: IStructuredSelection) => Option(ss.getFirstElement)
+      case _ => None
+    }
+
+  private def handleProofEntrySelected(event: ExecutionEvent, e: ProofEntry) {
+
+    val shell = HandlerUtil.getActiveShell(event)
+    
+    val dialog = new MarkFeaturesDialog(shell)
+    dialog.open()
+    
+  }
+
+}
---
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/features/MarkFeaturesDialog.scala
@@ -0,0 +1,13 @@
+package org.ai4fm.proofprocess.ui.features
+
+import org.eclipse.jface.dialogs.StatusDialog
+import org.eclipse.swt.widgets.Shell
+
+/**
+ * A dialog to mark various proof process features.
+ * 
+ * @author Andrius Velykis
+ */
+class MarkFeaturesDialog(parent: Shell) extends StatusDialog(parent) {
+
+}
