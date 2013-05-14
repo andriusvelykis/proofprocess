81ae21d9265eee27a31823ddd1f65120b817feb0
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-05-14 14:00:02 +0100

    compose in/out goals in proof elements (e.g. parallel or seq)

---
+++ org.ai4fm.proofprocess.core/src/org/ai4fm/proofprocess/core/store/ProofElemComposition.scala
@@ -0,0 +1,54 @@
+package org.ai4fm.proofprocess.core.store
+
+import scala.collection.JavaConverters._
+
+import org.ai4fm.proofprocess.{ProofDecor, ProofElem, ProofEntry, ProofParallel, ProofSeq, Term}
+
+
+/**
+ * Provides access to proof data in a compositional way,
+ * e.g. the "before" and "after" goals of Seq or Parallel elements.
+ * 
+ * @author Andrius Velykis
+ */
+object ProofElemComposition {
+
+  def composeEntries[A](elem: ProofElem, before: Boolean)(
+      f: ProofEntry => Seq[A]): Seq[A] = elem match {
+
+    case entry: ProofEntry => f(entry)
+
+    case decor: ProofDecor => composeEntries(decor.getEntry, before)(f)
+
+    case parallel: ProofParallel => {
+      // TODO review what to do with links? E.g. how about assumptions..?
+      val parEntries = parallel.getEntries.asScala //++ parallel.getLinks.asScala
+
+      if (parEntries.isEmpty) {
+        Nil
+      } else {
+        val results = parEntries map { e => composeEntries(e, before)(f) }
+        results.flatten
+      }
+    }
+
+    case seq: ProofSeq => {
+      val seqEntries = seq.getEntries.asScala
+
+      if (seqEntries.isEmpty) {
+        Nil
+      } else if (before) {
+        composeEntries(seqEntries.head, before)(f)
+      } else {
+        composeEntries(seqEntries.last, before)(f)
+      }
+    }
+  }
+
+  def composeInGoals(elem: ProofElem): Seq[Term] =
+    composeEntries(elem, true) { entry => entry.getProofStep.getInGoals.asScala }
+
+  def composeOutGoals(elem: ProofElem): Seq[Term] =
+    composeEntries(elem, false) { entry => entry.getProofStep.getOutGoals.asScala }
+
+}
