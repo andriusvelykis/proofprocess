f1e95a7c957783b428beada667345fb3eaa07d81
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-05-16 16:31:21 +0100

    avoid using OSGI Declarative Services to auto-start pp.isabelle.core tracking
    The OSGI DS usage messes up Eclipse plug-in startup sequence and an incorrect workspace is used (default instead of selected). Instead, use `org.eclipse.ui.startup` extension point to auto-start pp.isabelle.ui, which in turn starts pp.isabelle.core.

---
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/IsabellePProcessCore.scala
@@ -0,0 +1,16 @@
+package org.ai4fm.proofprocess.isabelle.core
+
+/**
+ * Isabelle ProofProcess plug-in facade.
+ * 
+ * @author Andrius Velykis
+ */
+object IsabellePProcessCore {
+  
+  def init() {
+    // do not do anything at the moment - this is used for plug-in activation:
+    // the plug-in gets activated when its classes are loaded,
+    // which in turns starts ProofProcess tracking
+  }
+
+}
