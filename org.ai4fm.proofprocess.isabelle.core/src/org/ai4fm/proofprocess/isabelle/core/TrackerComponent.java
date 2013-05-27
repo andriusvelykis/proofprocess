e15ad0b0c7140810e3bb35187c23a1bc5725fe0a
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-05-27 15:22:20 +0100

    Revert "unused"
    This reverts commit ef42588ac8f02a858374b1ba206e938623b42dd2.

---
+++ org.ai4fm.proofprocess.isabelle.core/src/org/ai4fm/proofprocess/isabelle/core/TrackerComponent.java
@@ -0,0 +1,5 @@
+package org.ai4fm.proofprocess.isabelle.core;
+
+public class TrackerComponent {
+
+}
---
+++ org.ai4fm.proofprocess.zeves.core/src/org/ai4fm/proofprocess/zeves/core/TrackerComponent.java
@@ -0,0 +1,5 @@
+package org.ai4fm.proofprocess.zeves.core;
+
+public class TrackerComponent {
+
+}
