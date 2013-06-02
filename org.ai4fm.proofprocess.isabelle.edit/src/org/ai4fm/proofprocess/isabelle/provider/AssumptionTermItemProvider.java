8ef14ffebe02c933f7b919e13ef54c51ed104320
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-06-02 12:08:41 +0100

    missing tag to avoid regeneration

--- org.ai4fm.proofprocess.isabelle.edit/src/org/ai4fm/proofprocess/isabelle/provider/AssumptionTermItemProvider.java
+++ org.ai4fm.proofprocess.isabelle.edit/src/org/ai4fm/proofprocess/isabelle/provider/AssumptionTermItemProvider.java
@@ -112,7 +112,7 @@ public class AssumptionTermItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
