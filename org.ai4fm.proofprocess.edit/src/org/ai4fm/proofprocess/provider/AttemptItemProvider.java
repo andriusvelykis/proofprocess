9dba41d291975ada49ba73e6b96235fda430a6dd
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-06-10 12:30:39 +0100

    better icon for attempt

--- org.ai4fm.proofprocess.edit/src/org/ai4fm/proofprocess/provider/AttemptItemProvider.java
+++ org.ai4fm.proofprocess.edit/src/org/ai4fm/proofprocess/provider/AttemptItemProvider.java
@@ -103,11 +103,11 @@ public class AttemptItemProvider
 	 * This returns Attempt.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/Attempt"));
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/Attempt.png"));
 	}
 
 	/**
