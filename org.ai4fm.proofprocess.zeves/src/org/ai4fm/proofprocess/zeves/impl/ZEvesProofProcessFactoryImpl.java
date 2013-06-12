eefeaa8b8b8cb0f172908364cf28e64551b12288
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2013-06-12 13:11:48 +0100

    bumped pp.zeves model to v1.0.0.12 (because core PP model changed)

--- org.ai4fm.proofprocess.zeves/src/org/ai4fm/proofprocess/zeves/impl/ZEvesProofProcessFactoryImpl.java
+++ org.ai4fm.proofprocess.zeves/src/org/ai4fm/proofprocess/zeves/impl/ZEvesProofProcessFactoryImpl.java
@@ -34,7 +34,7 @@ public class ZEvesProofProcessFactoryImpl extends EFactoryImpl implements ZEvesP
 	 */
 	public static ZEvesProofProcessFactory init() {
 		try {
-			ZEvesProofProcessFactory theZEvesProofProcessFactory = (ZEvesProofProcessFactory)EPackage.Registry.INSTANCE.getEFactory("http://org/ai4fm/proofprocess/zeves/v1.0.0.11"); 
+			ZEvesProofProcessFactory theZEvesProofProcessFactory = (ZEvesProofProcessFactory)EPackage.Registry.INSTANCE.getEFactory("http://org/ai4fm/proofprocess/zeves/v1.0.0.12"); 
 			if (theZEvesProofProcessFactory != null) {
 				return theZEvesProofProcessFactory;
 			}
