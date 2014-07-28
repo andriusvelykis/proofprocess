3d3bb1fc94b402103bade432c04d3f3560a91cd6
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2014-07-28 15:15:14 +0300

    support exporting some PP data to a plain text file

---
+++ org.ai4fm.proofprocess.core/src/org/ai4fm/proofprocess/core/print/PrintPlainText.scala
@@ -0,0 +1,174 @@
+package org.ai4fm.proofprocess.core.print
+
+import scala.collection.JavaConverters.asScalaBufferConverter
+import scala.util.Try
+
+import org.ai4fm.proofprocess.Attempt
+import org.ai4fm.proofprocess.Proof
+import org.ai4fm.proofprocess.ProofEntry
+import org.ai4fm.proofprocess.ProofFeature
+import org.ai4fm.proofprocess.ProofId
+import org.ai4fm.proofprocess.ProofInfo
+import org.ai4fm.proofprocess.ProofParallel
+import org.ai4fm.proofprocess.ProofSeq
+import org.ai4fm.proofprocess.Term
+
+
+/**
+ * Prints PP elements to plain text format (Markdown-like)
+ * 
+ * @author Andrius Velykis
+ */
+object PrintPlainText {
+
+  def print(elem: Any): String = elem match {
+
+    case attempt: Attempt =>
+      printProof(attempt) + "\n\n" +
+      print(attempt.getProof)
+
+    case info: ProofInfo => {
+      List(
+        Option(info.getIntent) map { _.getName } orElse Some(""),
+        printNarrative(info.getNarrative),
+        printLabelList("In Features:", info.getInFeatures.asScala),
+        printLabelList("Out Features:", info.getOutFeatures.asScala),
+        if (!info.getInFeatures.isEmpty || !info.getOutFeatures.isEmpty) Some("---") else None
+      ).flatten.mkString("\n\n")
+    }
+
+    case seq: ProofSeq => {
+      val seqStr = 
+        "### SEQ: " + print(seq.getInfo) ::
+        (seq.getEntries.asScala map print).toList
+      bullet(seqStr.mkString("\n\n"))
+    }
+
+    case par: ProofParallel => {
+      val parStr = 
+        "### PAR: " + print(par.getInfo) ::
+        (par.getEntries.asScala map print).toList
+      bullet(parStr.mkString("\n\n"))
+    }
+
+    case entry: ProofEntry => {
+      val entryStr = List(
+        Some("#### ENTRY: " + print(entry.getInfo)),
+        //print(entry.getProofStep),
+        printLabelList("\u2192 In Goals:", entry.getProofStep.getInGoals.asScala),
+        printLabelList("\u2190 Out Goals:", entry.getProofStep.getOutGoals.asScala, Some("Done"))
+      )
+      bullet(entryStr.flatten.mkString("\n\n"))
+    }
+
+    case link: ProofId => {
+      bullet("#### LINK: " + print(link.getInfo))
+    }
+
+    // TODO delegate somehow printing of terms?
+    case term: Term => printTerm(term)
+
+    case feature: ProofFeature => {
+      val featStr = List(
+        Option(feature.getName) map { _.getName } getOrElse "<?feature>",
+        "(",
+        (feature.getParams.asScala map print).mkString(", "),
+        ")"
+      ).mkString
+
+      List(
+        Some(featStr),
+        printNarrative(feature.getMisc)
+      ).flatten.mkString("\n\n")
+    }
+
+    case prf: Proof => {
+      val attemptsStr =
+        if (prf.getAttempts.size == 1) "1 attempt"
+        else prf.getAttempts.size + " attempts"
+
+      val label = "# Proofs of " + prf.getLabel + " (" + attemptsStr + ")"
+
+      val prfStr = label :: (prf.getAttempts.asScala.toList map print)
+      prfStr.mkString("\n\n")
+    }
+
+    case _ => "Printing not supported: " + elem.getClass
+  }
+
+  private def println(elem: Any): String =
+    if (elem != null) print(elem) + "\n" else ""
+
+  private def printOpt(opt: Option[String]): String = opt getOrElse ""
+
+  private def prependLines(prepend: String)(text: String): String =
+    prependLines(prepend, prepend)(text)
+  
+  private def prependLines(firstLine: String, prepend: String)(text: String): String =
+    firstLine + text.replace("\n", "\n" + prepend)
+  
+  private val INDENT = "  "
+
+  private def indent(text: String) = prependLines(INDENT)(text)
+
+  private def bullet(text: String) = prependLines("  - ", "    ")(text)
+
+  private def printNarrative(text: String): Option[String] =
+    Option(text) filterNot { _.isEmpty } map prependLines("> ")
+
+  private def printProof(attempt: Attempt): String = Option(attempt.eContainer) match {
+    case Some(prf: Proof) => {
+      List(
+        "## Proof attempt of " + prf.getLabel,
+        numbered(prf.getGoals.asScala),
+        "---"
+      ).mkString("\n\n")
+    }
+
+    case _ => "Unknown attempt"
+  }
+
+  private def numbered(goals: Seq[_]): String = {
+    val goalStrs = goals map print
+    val numbered = goalStrs.zipWithIndex
+
+    val printed = numbered map {
+      case (str, i) => prependLines(" " + String.valueOf(i + 1) + ". ", "    ")(str)
+    }
+    printed.mkString("\n\n")
+  }
+
+  private def printLabelList(label: String,
+                             elems: Seq[_],
+                             empty: => Option[String] = None): Option[String] =
+    if (elems.isEmpty) {
+      empty map { label + "\n\n" + _ }
+    } else {
+      val str = label + "\n\n" +
+                numbered(elems) 
+      Some(str)
+    }
+
+  private def printTerm(term: Term): String = invokeGetDisplay(term) match {
+    case Some(str) => str
+    case _ => "Cannot print term: " + term
+  }
+
+  private def invokeGetDisplay(term: Term): Option[String] = {
+    val clss = classes(term.getClass) 
+    val methods = clss flatMap { cls => Try(cls.getMethod("getDisplay")).toOption }
+
+    val result = methods.headOption flatMap { mthd => Try(mthd.invoke(term)).toOption }
+
+    result match {
+      case Some(str: String) => Some(str)
+      case _ => None
+    }
+  }
+
+  private def classes(cls: Class[_]): Stream[Class[_]] = {
+    val superCls = Option(cls.getSuperclass) map classes
+    cls #:: (superCls getOrElse Stream.empty)
+  }
+  
+}
---
+++ org.ai4fm.proofprocess.ui/src/org/ai4fm/proofprocess/ui/actions/ExportPlainTextHandler.scala
@@ -0,0 +1,60 @@
+package org.ai4fm.proofprocess.ui.actions
+
+import scala.collection.JavaConverters.asScalaIteratorConverter
+
+import org.ai4fm.proofprocess.core.print.PrintPlainText
+import org.eclipse.core.commands.AbstractHandler
+import org.eclipse.core.commands.ExecutionEvent
+import org.eclipse.core.commands.ExecutionException
+import org.eclipse.jface.viewers.IStructuredSelection
+import org.eclipse.swt.SWT
+import org.eclipse.swt.widgets.FileDialog
+import org.eclipse.ui.handlers.HandlerUtil
+
+
+/**
+ * @author Andrius Velykis
+ */
+class ExportPlainTextHandler extends AbstractHandler {
+
+  @throws(classOf[ExecutionException])
+  override def execute(event: ExecutionEvent): Object = {
+
+    // get the structured selection if available
+    val selection = Option(HandlerUtil.getCurrentSelection(event)) collect {
+      case ss: IStructuredSelection => ss
+    }
+    
+    val selected = selection map { _.iterator.asScala.toList }
+    
+    selected.toList.flatten foreach exportPlainText(event)
+
+    // return value is reserved for future APIs
+    null
+  }
+  
+  private def exportPlainText(event: ExecutionEvent)(prfElem: Any) {
+
+    val printed = PrintPlainText.print(prfElem)
+
+    val fileDialog = new FileDialog(HandlerUtil.getActiveShell(event), SWT.SAVE)
+    fileDialog.setText("Select File to Export ProofProcess Data as Plain Text")
+    fileDialog.setOverwrite(true)
+    fileDialog.setFilterExtensions(Array("*.txt", "*.*"))
+    fileDialog.setFileName("attempt.txt")
+    val savePath = Option(fileDialog.open())
+
+    savePath foreach { path =>
+      printToFile(new java.io.File(path))(p => {
+        p.println(printed)
+      })
+    }
+    
+  }
+
+  private def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+    val p = new java.io.PrintWriter(f, "UTF-8")
+    try { op(p) } finally { p.close() }
+  }
+  
+}
