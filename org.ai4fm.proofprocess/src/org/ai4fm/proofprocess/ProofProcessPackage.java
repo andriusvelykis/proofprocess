9acfe1cf107f4988a627c9022fac8fe3e7ab9a73
Andrius Velykis <andrius.velykis@newcastle.ac.uk>
2014-08-10 15:09:54 +0100

    bumped pp.core to version 1.0.0.14

--- org.ai4fm.proofprocess/src/org/ai4fm/proofprocess/ProofProcessPackage.java
+++ org.ai4fm.proofprocess/src/org/ai4fm/proofprocess/ProofProcessPackage.java
@@ -42,7 +42,7 @@ public interface ProofProcessPackage extends EPackage {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	String eNS_URI = "http://org/ai4fm/proofprocess/v1.0.0.13";
+	String eNS_URI = "http://org/ai4fm/proofprocess/v1.0.0.14";
 
 	/**
 	 * The package namespace name.
--- org.ai4fm.proofprocess/src/org/ai4fm/proofprocess/impl/ProofProcessFactoryImpl.java
+++ org.ai4fm.proofprocess/src/org/ai4fm/proofprocess/impl/ProofProcessFactoryImpl.java
@@ -32,7 +32,7 @@ public class ProofProcessFactoryImpl extends EFactoryImpl implements ProofProces
 	 */
 	public static ProofProcessFactory init() {
 		try {
-			ProofProcessFactory theProofProcessFactory = (ProofProcessFactory)EPackage.Registry.INSTANCE.getEFactory("http://org/ai4fm/proofprocess/v1.0.0.13"); 
+			ProofProcessFactory theProofProcessFactory = (ProofProcessFactory)EPackage.Registry.INSTANCE.getEFactory(ProofProcessPackage.eNS_URI);
 			if (theProofProcessFactory != null) {
 				return theProofProcessFactory;
 			}
